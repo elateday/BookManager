4c346bccf1560a298c575c41e4c592d387d342b3
elateday <elateday0817@gmail.com>
2018-08-21 13:11:48 +0900

    Make Book struct & check

---
+++ BookManager/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  BookManager
+//
+//  Created by KimHanCheol on 2018. 8. 21..
+//  Copyright © 2018년 KimHanCheol. All rights reserved.
+//
+
+struct Book {
+    var name: String
+    var genre: String
+    var author: String
+    
+    func bookPrint() {                  //입력 확인
+        print("Name : \(name)")
+        print("Genre : \(genre)")
+        print("Author : \(author)")
+    }
+}
